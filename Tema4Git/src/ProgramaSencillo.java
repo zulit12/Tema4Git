d1b8582d84a57966596eb64a1f4ea034d10416a6
Zulit <49183495+zulit12@users.noreply.github.com>
2021-05-25 09:09:12 +0200

    Un Programa Sencillo

---
+++ Tema4Git/src/ProgramaSencillo.java
@@ -0,0 +1,11 @@
+
+public class ProgramaSencillo {
+
+	public static void main(String[] args) {
+		System.out.println("Un programa Sencillo");
+		System.out.println("Esta asignatura la apruebo");
+		System.out.println("Por fin verano");
+
+	}
+
+}
